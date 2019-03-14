3e5ae763d19efe27ca96eb3b4a16cb5315ceaf58
Shreyas Tallamraju <shreyastallamraju2020@u.northwestern.edu>
2019-03-13 20:48:11 -0500

    added lawyer link again

--- js/login.js
+++ js/login.js
@@ -23,7 +23,7 @@ function navBarInjection() {
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="` + url + `index.html">Home</a></li>
-        <li><a href="#">Lawyers</a></li>
+        <li><a href="` + url + `lawyers.html">Lawyers</a></li>
         <li><a href="` + url + `resources.html">Resources</a></li>
         <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
